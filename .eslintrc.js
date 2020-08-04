44ad2ef2075e4219969f7af5e02441e52a070142
Georgios Petrousis <georgios.petrousis@nentgroup.com>
2020-08-04 09:11:27 +0200

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,28 @@
 module.exports = {
+	env: {
+		node: true,
+		es2020: true
+	},
 	extends: [
-	  'airbnb',
-	  'eslint:recommended',
-	  'plugin:import/errors',
+		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings'
 	],
 	plugins: [
 	],
 	rules: {
-	},
-  };
+		'strict': 'off',
+		'no-undef': 1,
+		'nonblock-statement-body-position': ['error', 'beside'],
+		'curly': ['error', 'multi-line'],
+		'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+		'no-unused-expressions': ['error', { 'allowShortCircuit': true, 'allowTernary': true }],
+		'no-console': 'on',
+		'arrow-parens': ['error', 'as-needed'],
+		'object-curly-spacing': ['error', 'always'],
+		'comma-dangle': ['warn', 'never'],
+		'max-params': ['error', 3],
+		'no-multiple-empty-lines': 'error'
+	}
+}
